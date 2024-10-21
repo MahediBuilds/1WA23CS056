1e1c0167ad8ea3ec3ac0a64331de186c1996e498
Zain Mahedi <zainmahedi.cs23@bmsce.ac.in>
2024-10-21 22:58:51 +0530

    Create Stack.c

---
+++ Stack.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 5
+
+char stack[MAX];
+int top = -1;
+
+void push(char);
+char pop();
+void display();
+
+int main(void) {
+    char choice, value;
+
+    printf("Welcome to Stack Operations\n");
+    while (1) {
+
+        printf("1. Push\n2. Pop\n3. Display\n4. Exit\n");
+        printf("Enter your choice: ");
+        scanf(" %c", &choice);  
+
+        if (choice == '1') {
+            printf("Enter the value : ");
+            scanf(" %c", &value);  
+            push(value);
+
+        } else if (choice == '2') {
+            pop();
+
+        } else if (choice == '3') {
+            display();
+
+        } else if (choice == '4') {
+            exit(0);
+        } else {
+            printf("Invalid choice\n");
+        }
+    }
+}
+
+void push(char ch) {
+    if (top == MAX - 1) {
+        printf("Stack Overflow\n");
+        return;
+
+    } else {
+        stack[++top] = ch;
+    }
+}
+
+char pop() {
+    if (top == -1) {
+        printf("Stack Underflow\n");
+        return -1;
+
+    } else {
+        return stack[top--];
+    }
+}
+
+void display() {
+    if (top == -1) {
+        printf("Stack Underflow\n\n");
+        return;
+
+    } else {
+        printf("Stack : ");
+        for (int i = top; i >= 0; i--) {
+            printf("%c ", stack[i]);
+        }
+        printf("\n");
+    }
+}
